c00a3ce8054c1a2ea9ef0624a20ad51ae77e2788
kostyavolkov <volkovgoods@gmail.com>
2024-07-22 23:32:21 +0200

    1111

--- src/components/Process2/index.jsx
+++ src/components/Process2/index.jsx
@@ -17,21 +17,21 @@ const Process2 = () => {
             <div className="item md-mb50">
               <h4>02</h4>
               <h5>Supervision</h5>
-              <p>Nulla metus metus ullamcorper vel tincidunt sed euismod.</p>
+              <p>The works are constantly supervised by our designer and the foreman to make sure the concept is followed thoroughly. We value your and our time, so the process is planned efficiently and there are no gaps between construction stages. </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="item sm-mb50">
               <h4>03</h4>
               <h5>Budget Planning</h5>
-              <p>Nulla metus metus ullamcorper vel tincidunt sed euismod.</p>
+              <p>A detailed estimate of costs is provided for your approval. We work closely with many suppliers and manufacturers around Europe: European quality standards as well as competitive prices are guaranteed. </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="item">
               <h4>04</h4>
               <h5>Build a home for you</h5>
-              <p>Nulla metus metus ullamcorper vel tincidunt sed euismod.</p>
+              <p>We take care of the final touches: furniture and appliance installation is in our hands. You simply have to come and live in your new home. </p>
             </div>
           </div>
         </div>
