57e924a049429c974b7af1850238e931f0362ff5
kostyavolkov <volkovgoods@gmail.com>
2024-07-28 02:13:02 +0200

    11

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6d680d793f3f7de0"; }
+/******/ 		__webpack_require__.h = function() { return "5873ab54fe2c5875"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
